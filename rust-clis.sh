ac500cb5e7a79485e5b3d77d610b89138c3d95ee
Matt McCormick <matt.mccormick@kitware.com>
2020-12-18 20:43:09 -0500

    Install ripgrep, fuzzyfinder

--- rust-clis.sh
+++ rust-clis.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-cargo install lsd hunter dust fd-find bat bottom hyperfine sd tokei viu
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+cargo install ripgrep broot lsd hunter dust fd-find bat bottom hyperfine sd tokei viu
