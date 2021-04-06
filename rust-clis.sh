0be7c46456ef747fe4b7461665f1ab0b82ec064c
Matt McCormick <matt.mccormick@kitware.com>
2021-04-06 17:15:51 -0400

    Additional rust cli's, brew packages

--- rust-clis.sh
+++ rust-clis.sh
@@ -3,4 +3,4 @@
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
-cargo install ripgrep broot lsd hunter dust fd-find bat bottom hyperfine sd tokei viu
+cargo install ripgrep broot lsd hunter dust fd-find bat bottom hyperfine sd tokei viu git-interactive-rebase-tool
